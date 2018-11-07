9cb05ee84d963806111fd237b7f030cb29420ba6
Khartaz <tjaygallery@gmail.com>
2018-11-07 20:40:01 +0100

    new

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -1,14 +1,48 @@
 package com.crud.tasks.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import javax.persistence.*;
 
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
+@Entity(name = "tasks")
 public class Task {
     private Long id;
     private String title;
     private String content;
+
+    public Task() {
+    }
+
+    public Task(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID")
+    public Long getId() {
+        return id;
+    }
+
+    @Column(name = "NAME")
+    public String getTitle() {
+        return title;
+    }
+
+    @Column(name = "DESCRIPTION")
+    public String getContent() {
+        return content;
+    }
+
+    private void setId(Long id) {
+        this.id = id;
+    }
+
+    private void setTitle(String title) {
+        this.title = title;
+    }
+
+    private void setContent(String content) {
+        this.content = content;
+    }
 }
--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -1,14 +1,40 @@
 package com.crud.tasks.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
 public class TaskDto {
     private Long id;
     private String title;
     private String content;
+
+    public TaskDto() {
+
+    }
+    public TaskDto(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    private void setId(Long id) {
+        this.id = id;
+    }
+
+    private void setTitle(String title) {
+        this.title = title;
+    }
+
+    private void setContent(String content) {
+        this.content = content;
+    }
 }
