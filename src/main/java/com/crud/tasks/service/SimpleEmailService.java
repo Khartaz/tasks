ac8ce62a29b13e01be683466082ab48d897be140
tjanicki <tjaygallery@gmail.com>
2019-01-18 20:00:36 +0100

    emai

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -7,14 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class SimpleEmailService {
     @Autowired
     private JavaMailSender javaMailSender;
+    @Autowired
+    private MailCreatorService mailCreatorService;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SimpleMailMessage.class);
 
@@ -23,7 +25,7 @@ public class SimpleEmailService {
         LOGGER.info("Starting email preparation...");
         try {
 
-            javaMailSender.send(createMailMessage(mail));
+            javaMailSender.send(createMimeMessage(mail));
             LOGGER.info("Email has been sent.");
         } catch (MailException e) {
 
@@ -31,14 +33,23 @@ public class SimpleEmailService {
         }
     }
 
+    private MimeMessagePreparator createMimeMessage(final Mail mail) {
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
+    }
+
     private SimpleMailMessage createMailMessage(final Mail mail) {
 
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
-        mailMessage.setText(mail.getMessage());
+        mailMessage.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()));
 
-        Optional.ofNullable(mail.getToCc()).ifPresent(mailMessage::setCc);
+        //Optional.ofNullable(mail.getToCc()).ifPresent(mailMessage::setCc);
 
         return mailMessage;
     }
